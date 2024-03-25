ce0cdad2ef0eec5321cca0a628b98f56b296be4f
Kassy Raymond <kassyraymond2@gmail.com>
2024-03-25 13:45:01 -0400

    Added file path to processed so that the data can be loaded to the GraphDB

--- etl/extract/extract_GBADs_S3.py
+++ etl/extract/extract_GBADs_S3.py
@@ -2,4 +2,4 @@ from extract_helpers import s3Helpers as s3h
 
 resource = s3h.create_resource('aws-config.txt')
 
-s3h.get_all_objects(resource, 'gbads-tables', 's3-gbads-tables.json')
+s3h.get_all_objects(resource, 'gbads-tables', '../../data/processed/s3-gbads-tables.json')
