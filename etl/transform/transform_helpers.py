f057cdbeb07bdd3be23703db739403745c19e3c2
Kassy Raymond <kassyraymond2@gmail.com>
2024-03-25 14:26:13 -0400

    Moved functions from transform_faostat to transform_helpers since they are general use

--- etl/transform/transform_helpers.py
+++ etl/transform/transform_helpers.py
@@ -1,4 +1,56 @@
 import json
+import pandas as pd
+
+def get_cat_yr_area(df, cat_col, area_col, year_col):
+    """
+    Extracts unique categories and the years and countries that they occur in from the 'Item' column of the input DataFrame.
+
+    Parameters:
+    df (pd DataFrame): Input DataFrame containing columns named 'Item', 'Year', and 'Area'.
+
+    Returns:
+    pd DataFrame: DataFrame containing unique categories extracted from the 'Item' column and the years and countries that they occur in.
+    """
+
+    df_cat_yr_area = df.groupby([cat_col, area_col])[year_col].unique().apply(list).reset_index()
+    
+    df_cat_yr_area.columns = ['category','area','year']
+
+    return(df_cat_yr_area)
+
+def get_cat_yr(df, cat_col, year_col):
+    """
+    Extracts unique categories and the years that they occur in from the 'Item' column of the input DataFrame.
+
+    Parameters:
+    df (pd DataFrame): Input DataFrame containing columns named 'Item' and 'Year'.
+
+    Returns:
+    pd DataFrame: DataFrame containing unique categories extracted from the 'Item' column and the years that they occur in.
+    """
+    
+    df_cat_yrs = df.groupby([cat_col])[year_col].unique().apply(list).reset_index()
+
+    df_cat_yrs.columns = ['category','year']
+    
+    return(df_cat_yrs)
+
+def get_cat(df, col_name):
+    """
+    Extracts unique categories from the 'Item' column of the input DataFrame.
+
+    Parameters:
+    df (pd DataFrame): Input DataFrame containing a column named 'Item'.
+
+    Returns:
+    pd DataFrame: DataFrame containing unique categories extracted from the 'Item' column.
+    """
+
+    cats = df[col_name].unique()
+
+    df_cats = pd.DataFrame(cats, columns = ['category'])
+
+    return(df_cats)
 
 def load_from_json(json_file_path):
 
