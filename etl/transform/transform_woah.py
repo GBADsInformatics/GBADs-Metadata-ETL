e0edaf6ecae7b7d97f4e366fdda47b1577edb2f9
Kassy Raymond <kassyraymond2@gmail.com>
2024-03-27 05:58:56 -0400

    Code to create parent/child relationships

--- etl/transform/transform_woah.py
+++ etl/transform/transform_woah.py
@@ -5,23 +5,36 @@ import sys
 from pydantic import ValidationError
 from validators import url
 import transform_helpers as th
-from validations import validate_metadata as vm
+import validate_metadata as vm
 
 file_path = sys.argv[1]
 metadata_path = sys.argv[2]
-org_path = sys.argv[3]
-out_path = sys.argv[4]
+out_path = sys.argv[3]
 
 # Get woah species, temporal cov and spatial coverage 
 df = pd.read_csv(file_path)
 
 temporalCoverage = th.get_temporal_coverage(df, 'year')
 spatialCoverage = th.get_cat(df, 'country')
-species = th.get_species(df, 'species')
+species = th.get_cat(df, 'species')
 
 # Read in metadata file and add in spatial coverage, temporal coverage, species 
 metadata = th.load_from_json(metadata_path)
 metadata['species'] = species
 metadata['spatialCoverage'] = spatialCoverage
 metadata['temporalCoverage'] = temporalCoverage
+
+# Create country, parent df
+area_parent = th.get_cat_yr(df, 'country', 'year')
+area_parent['parent'] = metadata['sourceTable']
+
+# Rename cols 
+area_parent.columns = ['Value','year','parent']
+
+# Create category, parent df
+category_parent = th.get_cat_yr(df, 'species', 'year')
+category_parent['parent'] = metadata['sourceTable']
+
+# Rename cols
+category_parent.columns = ['Value','year','parent']
 
