ca752fecf497f8cca4c24e048740347cbf42ca53
Kassy Raymond <kassyraymond2@gmail.com>
2024-03-28 13:32:40 -0400

    Created a script that takes in a file path to the json file that contains the data that will be used to update dataset nodes

---
+++ etl/load/update_dataset.py
@@ -0,0 +1,13 @@
+import load_data_helpers as dh
+import pandas as pd
+import json
+from database import get_db_driver
+import sys
+
+driver = get_db_driver()
+metadata_path = sys.argv[1]
+
+with open(metadata_path, 'r') as file:
+    metadata = json.load(file)
+
+dh.update_dataset(metadata, driver)
