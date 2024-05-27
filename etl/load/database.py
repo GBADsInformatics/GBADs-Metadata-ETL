580abc87db52184c467944c7fa2ec861afb5f41b
Kassy Raymond <kassyraymond2@gmail.com>
2024-05-27 10:35:16 -0400

    Added config-test.ini to gitignore and updated database.py to include test functionality

--- etl/load/database.py
+++ etl/load/database.py
@@ -5,14 +5,21 @@ from neo4j import GraphDatabase
 from load_data_helpers import read_db_config
 
 class Database:
-    def __init__(self):
+    def __init__(self, test=False):
         if os.environ.get('GRAPHDB_USERNAME') is None:
-            config = read_db_config()
+            if test:
+                config = read_db_config(filename = 'config-test.ini')
+            else:
+                config = read_db_config()
             self.driver = GraphDatabase.driver(config['uri'], auth=(config['username'], config['password']))
         else:
             self.driver = GraphDatabase.driver(os.environ.get('GRAPHDB_URI', None), auth=(os.environ.get('GRAPHDB_USERNAME', None), os.environ.get('GRAPHDB_PASSWORD', None)))
 
 database = Database()
 
-def get_db_driver():
+test_database = Database(test=True)
+
+def get_db_driver(test=False):
+    if test:
+        return test_database.driver
     return database.driver
