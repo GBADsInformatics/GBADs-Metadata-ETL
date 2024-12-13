e62a45756a97ec6236f1c77e2b62ec32e16e3f51
Kassy Raymond <kassyraymond2@gmail.com>
2024-12-13 15:09:56 -0500

    Adding in a script that uses the python neo4j_backup package to dump the neo4j db. I did this in response to an issue I had with accessing snapshots from the neo4j aura console.

---
+++ etl/backup/extract.py
@@ -0,0 +1,21 @@
+from neo4j import GraphDatabase
+from neo4j_backup import Extractor
+import sys
+sys.path.append("../load")
+from database import get_db_driver
+
+if __name__ == "__main__":
+
+    driver = get_db_driver()
+
+    database = "neo4j"
+
+    project_dir = "data_dump"
+    input_yes = False
+    compress = True
+    indent_size = 4  # Indent of json files
+    json_file_size: int = int("0xFFFF", 16)  # Size of data in memory before dumping
+    extractor = Extractor(project_dir=project_dir, driver=driver, database=database,
+                          input_yes=input_yes, compress=compress, indent_size=indent_size,
+                          pull_uniqueness_constraints=True)
+    extractor.extract_data()
