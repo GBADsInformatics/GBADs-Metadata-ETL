2c365a984541467586a5374b09103a2cef09b60d
Kassy Raymond <kassyraymond2@gmail.com>
2024-12-16 15:07:40 -0500

    Added ability to connect to a dev instance

--- etl/load/database.py
+++ etl/load/database.py
@@ -5,10 +5,12 @@ from neo4j import GraphDatabase
 from load_data_helpers import read_db_config
 
 class Database:
-    def __init__(self, test=False):
+    def __init__(self, test=False, dev=False):
         if os.environ.get('GRAPHDB_USERNAME') is None:
             if test:
-                config = read_db_config(filename = 'config-test.ini')
+                config = read_db_config(filename = 'config-test.ini')           
+            elif dev:
+                config = read_db_config(filename = 'config-dev.ini')
             else:
                 config = read_db_config()
             self.driver = GraphDatabase.driver(config['uri'], auth=(config['username'], config['password']))
@@ -16,10 +18,12 @@ class Database:
             self.driver = GraphDatabase.driver(os.environ.get('GRAPHDB_URI', None), auth=(os.environ.get('GRAPHDB_USERNAME', None), os.environ.get('GRAPHDB_PASSWORD', None)))
 
 database = Database()
-
 test_database = Database(test=True)
+dev_database = Database(dev=True)
 
-def get_db_driver(test=False):
+def get_db_driver(dev=False, test=False):
+    if dev:
+        return dev_database.driver
     if test:
         return test_database.driver
     return database.driver
